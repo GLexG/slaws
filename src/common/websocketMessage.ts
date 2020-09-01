46c8cec22c13c6aaa4a84c961d4dc204695f2299
Leon Gradisar <Leon_Gradisar@neimanmarcus.com>
2020-09-01 15:40:50 +0200

    extended dynamo db connection data

--- src/common/websocketMessage.ts
+++ src/common/websocketMessage.ts
@@ -3,14 +3,13 @@ const AWS = require('aws-sdk');
 const create = (domainName, stage) => {
     const endpoint = `${domainName}/${stage}`;
 
-    //setting up aws api gateway so we can send messages through websoket
+    //setting up aws api gateway so we can send messages through websocket
     return new AWS.ApiGatewayManagementApi({
         apiVersion: '2018-11-29',
         endpoint,
     });
 };
 
-//check connectionId here
 export const send = ({domainName, stage, connectionID, message}) => {
     const ws = create(domainName, stage);
 
--- src/websockets/connect.ts
+++ src/websockets/connect.ts
@@ -4,10 +4,8 @@ import {APIGatewayProxyHandler} from "aws-lambda";
 
 // LOAD env variable from config file
 const tableName = process.env.tableName;
-// const tableName = 'WebsocketUsers';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-// exports.handler = async event => {
     console.log('event', event);
     console.log('table name: ', tableName)
 
@@ -15,6 +13,8 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
     const data = {
         ID: connectionID,
+        nm_id: event.queryStringParameters.nmId,
+        correlation_id: event.queryStringParameters.correlationId,
         date: Date.now(),
         messages: [],
         domainName,
